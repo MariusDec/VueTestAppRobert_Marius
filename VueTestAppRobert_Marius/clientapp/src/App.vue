5f77b1526938f92e74d7b7ed3c4ab80510270f80
Robert Gogete <gogeterobert@yahoo.com>
2020-09-24 19:07:03 +0300

    am adaugat niste butoane si un mesaj

--- VueTestAppRobert_Marius/clientapp/src/App.vue
+++ VueTestAppRobert_Marius/clientapp/src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Login :msg="msg" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Login from './components/Login.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Login
+        },
+        data() {
+            return {
+                msg: "Marius!"
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ VueTestAppRobert_Marius/clientapp/src/components/Login.vue
@@ -0,0 +1,24 @@
+﻿<template>
+    <div>
+        <div>{{msg}}</div>
+        <div>Choose login type</div>
+        <button data-v-3ecc9273="" type="button" class="md-button md-raised md-primary md-theme-demo-light">
+            <div class="md-ripple">
+                <div class="md-button-content">Contractor</div>
+            </div>
+        </button>
+        <button data-v-3ecc9273="" type="button" class="md-button md-raised md-primary md-theme-demo-light">
+            <div class="md-ripple">
+                <div class="md-button-content">Admin</div>
+            </div>
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            msg: String
+        }
+    }
+</script>
